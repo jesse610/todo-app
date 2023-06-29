8b9dad0cd38802fe6831af626f46bfb3d4116760
Jesse <jvelas028@ucr.edu>
2023-06-29 16:19:20 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { createTodoItem, createProject, projects, setProject, removeProject, removeTodoItem, editDueDate, editTitle, editDescription, editPriority, editProjectName, markTodoItemComplete, currentActiveProject } from "./applicationLogic";
+import { createTodoItem, createProject, projects, setProject, removeProject, removeTodoItem, editDueDate, editTitle, editDescription, editPriority, editProjectName, markTodoItemComplete, currentActiveProject, pushTodoItem, sortTodos } from "./applicationLogic";
 import { displayProjects, displayTasks, displayTaskForm, addProject, displayProjectBtns, displaySorted } from "./domLogic";
 import './style.css'
-
-createTodoItem('Test title', 'Test descr', '06/28/23', 'High')
+createTodoItem('Wash clothes', 'asdfe', '07/10/23', 'low')
+createTodoItem('Test title', 'Test descr', '06/29/23', 'High')
 createTodoItem('Wash clothes', 'asdfe', '07/10/23', 'low')
 createProject('test 2')
 createProject('test 3')
-console.log(projects[currentActiveProject][0])
+// console.log(projects[currentActiveProject][0])
+createTodoItem('clean car', 'rewef', '06/29/23', 'medium')
+editProjectName('test 2', 'changed Name!')
 
 displayProjects()
 displayTasks()
@@ -15,4 +17,5 @@ addProject()
 displayProjectBtns()
 displaySorted()
 console.log(projects)
+sortTodos('today')
 
