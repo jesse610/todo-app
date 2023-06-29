328371fc53f65965bb806f8da6f5518bb2732c27
Jesse <jvelas028@ucr.edu>
2023-06-28 18:19:25 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import { createTodoItem, createProject, projects, setProject, removeProject, removeTodoItem, editDueDate, editTitle, editDescription, editPriority, editProjectName, markTodoItemComplete, currentActiveProject } from "./applicationLogic";
-import { displayProjects, displayTasks, displayTaskForm, addProject, displayRemoveProjectBtns } from "./domLogic";
+import { displayProjects, displayTasks, displayTaskForm, addProject, displayProjectBtns, displaySorted } from "./domLogic";
 import './style.css'
 
 createTodoItem('Test title', 'Test descr', '06/28/23', 'High')
 createTodoItem('Wash clothes', 'asdfe', '07/10/23', 'low')
+createProject('test 2')
+createProject('test 3')
 console.log(projects[currentActiveProject][0])
 
 displayProjects()
 displayTasks()
 displayTaskForm()
 addProject()
-displayRemoveProjectBtns()
+displayProjectBtns()
+displaySorted()
 console.log(projects)
 
